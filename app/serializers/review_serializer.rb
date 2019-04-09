7ab5596f860fc9602743f02254535eb742c01b1d
Yazed Jamal <muyaszed@gmail.com>
2019-04-09 10:01:13 +0100

    updated review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,9 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :created_at, :updated_at, :user_id
+  attributes :id, :restaurant_id, :comment, :created_at, :updated_at, :user_id, :restaurant_name
   belongs_to :user, serializer: UserShortSerializer
   belongs_to :restaurant
+
+  def restaurant_name
+    self.object.restaurant.name
+  end
 end
