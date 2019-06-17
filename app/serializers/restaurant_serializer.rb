63dc7d2921fe13f3625d67693d351f0e26e10102
Yazed Jamal <muyaszed@gmail.com>
2019-06-17 12:35:58 +0100

    added time stamp for restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,10 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :city, :country, :postcode, :category, :cuisine, :start, :desc, :cover_uri, :created_at, :end, :latitude, :longitude, :location, :web
+  attributes :id, :name, :address, :city, :country, :postcode, :category, :cuisine, :start, :desc, :cover_uri, :created_at, :end, :latitude, :longitude, :location, :web, :updated_at
   has_many :bookmarking_user, serializer: UserBookmarkSerializer
   has_many :checking_ins, serializer: UserCheckinSerializer
+
+
+  def updated_at
+    self.object.updated_at.to_i
+  end
 end
