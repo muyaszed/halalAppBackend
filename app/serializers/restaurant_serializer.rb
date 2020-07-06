8505a654607b53b2b363d17861f3c67699464853
Yazed Jamal <muyaszed@gmail.com>
2020-07-06 22:56:39 +0100

    Added review serialzer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -2,6 +2,7 @@ class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :city, :country, :postcode, :category, :cuisine, :start, :desc, :cover_uri, :created_at, :end, :latitude, :longitude, :location, :web, :updated_at
   has_many :bookmarking_user, serializer: UserBookmarkSerializer
   has_many :checking_ins, serializer: UserCheckinSerializer
+  has_many :reviews, serializer: UserReviewSerializer
 
 
   def updated_at
