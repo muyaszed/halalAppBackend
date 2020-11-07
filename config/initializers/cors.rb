a8b30bfb1a2f4d681e9f48fe3d312f31cdffad1a
Yazed Jamal <muyaszed@gmail.com>
2020-11-07 14:51:25 +0000

    Changed CORS settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://gothalal-admin.web.app/']
+    origins ['https://gothalal-admin.web.app/', 'https://gothalal-admin.firebaseapp.com/']
 
     resource '*',
       headers: :any,
